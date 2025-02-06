c8b91d28a32e445fbe2f985c8a6df4f3c2c5d79b
clu <clu@wandrers.one>
2025-02-06 20:41:14 +0800

    Init routing api

---
+++ routing-api/src/test/java/idv/clu/api/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package idv.clu.api;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
